a87ce811176df8ce5e661bd161be9b999aa46f0e
huzaifa2213 <huzaifa.kamran1322@gmail.com>
2021-12-27 23:31:54 +0500

    About Us Done

--- src/components/home/SupportUs.jsx
+++ src/components/home/SupportUs.jsx
@@ -4,34 +4,50 @@ import {MdPeopleAlt , MdFastfood} from 'react-icons/md'
 import {BsGraphUp , BsEmojiLaughing} from 'react-icons/bs'
 import {GiArmorPunch} from 'react-icons/gi'
 import BasicImage from '../../assets/image2.jpg';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import publicRequest from '../../requestMethod';
 
 
 
 const SupportUs = () =>{
+ const [secondSection, setSecondSection] = useState([]);
+
+    useEffect(()=>{
+    const getSecondSection = async () =>{
+        try {
+        const res = await publicRequest.get('frontend/secondsection');
+        const [resSection] = res.data.data;
+        setSecondSection(resSection);
+         }catch(err){
+            console.log(err);
+        }
+    };
+    getSecondSection();
+    },[])
     return(
         <div className="support-us">
          <Container>
                 <Row>
                     <Col xs="12" md="12">
-                        <h5>You can join Us</h5>
-                        <h3>What We Are Doing</h3>
+                        <h3>{secondSection.mainHeading}</h3>
                         <Row className='content'>
                            
                             <Col xs="12" md="6" className="each-content">
-                                <h6> <span> <BsGraphUp size={25}/> </span> Education</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, non nesciunt ad.</p>
+                                <h6> <span> <BsGraphUp size={25}/> </span> {secondSection.headin1}</h6>
+                                <p>{secondSection.text1}</p>
                             </Col>
                             <Col xs="12" md="6" className="each-content">
-                                <h6> <span> <BsEmojiLaughing size={25}/> </span> Education</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, non nesciunt ad.</p>
+                                <h6> <span> <BsEmojiLaughing size={25}/> </span> {secondSection.heading2}</h6>
+                                <p>{secondSection.text3}</p>
                             </Col>
                             <Col xs="12" md="6" className="each-content">
-                                <h6> <span> <MdFastfood size={25}/> </span> Education</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, non nesciunt ad.</p>
+                                <h6> <span> <MdFastfood size={25}/> </span> {secondSection.heading3}</h6>
+                                <p>{secondSection.text3}</p>
                             </Col>
                             <Col xs="12" md="6" className="each-content">
-                                <h6> <span> <GiArmorPunch size={25}/> </span> Education</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, non nesciunt ad.</p>
+                                <h6> <span> <GiArmorPunch size={25}/> </span> {secondSection.heading4}</h6>
+                                <p>{secondSection.text4}</p>
                             </Col>
                                
                         </Row>
